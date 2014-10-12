c14dc7424ecc3d3a4361762ca95a386e1efc3d02
Mathias Helsen <helsen.mathias@gmail.com>
2014-10-12 09:38:50 +0200

    Before the code was structured to represent a Fast Multipole tree setup, now we will try to create a kd-tree. Moreover, to implement a nearest neighbour (NN) search and a n-NN search perhaps as well.

--- drunken_octo.hpp
+++ drunken_octo.hpp
@@ -35,24 +35,25 @@ private:
     // Each drunken_octo node ofcourse needs to be able to carry some information
     T nodeData;
     // Each node possibly has extents in any of its N dimensions
-    S nodeExtents[2];
+    S nodePosition;
     // Each node has a parent, unless it is root (then it is NULL)
     drunken_octo<T, S> *parent;
     // Each node can have some children
-    drunken_octo<T, S> **children;
+    drunken_octo<T, S> *children[2];
     // The number of dimensions 
     int N;
-    int nChildren;
+    // The depth at which the node is located
+    int depth;
     // Is this node a leaf?
     bool leaf;
-    // The function to determine at which quadrant/octant/... a child belongs
-    // and set the new extents of the new node
-    int (* tant)( drunken_octo<T, S> *parent, drunken_octo<T, S> *child );
+    // A function pointer to compare the values of two nodes according to a specific
+    // divisional hyperplane indicated by the rank
+    int (* compare)( drunken_octo<T, S> *A, drunken_octo<T, S> *B, int rank);
 public:
     // Adding a node requires only a datapoint, a comparison function and a function to yield the extents of a node
     drunken_octo ( 
 	T *dataPoint, 
-	int (* _tant)( drunken_octo<T, S> *parent, drunken_octo<T, S> *child ),
+	int (* _compare)( drunken_octo<T, S> *A, drunken_octo<T, S> *B, int rank),
 	int _N );
     // Remove a node, and its children!
     ~drunken_octo();
@@ -69,40 +70,36 @@ public:
 
 template <class T, class S> drunken_octo<T, S>::drunken_octo ( 
 	T *dataPoint, 
-	int (* _tant)( drunken_octo<T, S> *parent, drunken_octo<T, S> *child ),
+	int (* _compare)( drunken_octo<T, S> *A, drunken_octo<T, S> *B, int rank),
 	int _N )
 {
     memcpy( &nodeData, dataPoint, sizeof(T) );
-    tant = _tant;
+    compare = _compare;
     N = _N;
-    nChildren = 1;
+    depth = 0;
     leaf = true;
-    for(int i = 0; i < N; i++)
-    {
-	nChildren *= 2;
-    }
 
-    children = new drunken_octo<T, S> *[nChildren];
-    for(int i = 0; i < nChildren; i++ )
-    {
-	children[i] = NULL;
-    }
+    children[0] = NULL;
+    children[1] = NULL;
 }
 
 template <class T, class S> drunken_octo<T, S>::~drunken_octo()
 {
-    for(int i = 0; i < nChildren; i++)
+    if(children[0] != NULL)
     {
-	if(children[i] != NULL)
-	{
-	    delete children[i];
-	}
+	delete children[0];
     }
+    if(children[1] != NULL)
+    {
+	delete children[1];
+    }
+
     delete[] children;
 }
 
 template <class T, class S> void drunken_octo<T, S>::addNode( drunken_octo<T, S> *newNode )
 {
+/*
     int tantIndex = tant( this, newNode );
     if( children[tantIndex] != NULL )
     {
@@ -117,22 +114,19 @@ template <class T, class S> void drunken_octo<T, S>::addNode( drunken_octo<T, S>
     {
 	leaf = false;
     }
-}
-
-template <class T, class S> void drunken_octo<T, S>::getExtents( S *extents )
-{
-    memcpy( extents, nodeExtents, sizeof(S)*2);
-}
-
-template <class T, class S> void drunken_octo<T, S>::setExtents( S *extents )
-{
-    memcpy( nodeExtents, extents, sizeof(S)*2);
+*/
 }
 
 template <class T, class S> void drunken_octo<T, S>::getData( T *data )
 {
     memcpy(data, &nodeData, sizeof(T));
 }
+
+template <class T, class S> void drunken_octo<T, S>::getPosition( S *position )
+{
+    memcpy(position, &nodePosition, sizeof(S));
+}
+
 
 template <class T, class S> void drunken_octo<T, S>::getLeaves( std::vector<drunken_octo<T, S> *> *leavesList )
 {
@@ -142,7 +136,7 @@ template <class T, class S> void drunken_octo<T, S>::getLeaves( std::vector<drun
     }
     else
     {
-	for(int i = 0; i < nChildren; i++)
+	for(int i = 0; i < 2; i++)
 	{
 	    if(children[i] != NULL)
 	    {
